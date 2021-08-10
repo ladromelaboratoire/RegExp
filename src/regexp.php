09db4022dc3d1a262e87113485a9e42b63dba73c
EMMANUEL HAVET <ehavet@ladrome.fr>
2021-08-10 08:49:23 +0200

    Aesthetic changes

--- src/regexp.php
+++ src/regexp.php
@@ -33,7 +33,7 @@ class regexp {
 	public const IBAN_PL = '^PL[ ]?\d{2}[ ]?\d{3}[ ]?\d{4}[ ]?\d{1}[ ]?\d{16}$';
 	public const IBAN_SE = '^SE[ ]?\d{2}[ ]?\d{3}[ ]?\d{17}$';
 	
-	//Date Time
+	// Date Time
 	public const HM24_INTL = '^(?:[01]?[0-9]|2[0-3]):[0-5][0-9]$';
 	public const HMS24_INTL = '^(?:[01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$';
 	
@@ -43,14 +43,14 @@ class regexp {
 	
 	// Phone
 	public const PHONE_INTL_RAW = '^(\+[1-9][0-9]{0,2}|[0]){1}[1-9][0-9]{7,12}$';
-	public const PHONE_INTL_FRIENDLY = '^(?:0|\+)(?:[\d \(\)\-\.]){6,19}(?:\d){1}$'; //+11 (0)12.3456-789
-	public const PHONE_FR = '^(?:0|\+33[ ]?)(?:\d[ \.]?)(?:\d\d[ \.]?){3}(?:\d\d)$'; //0123456789 or 01 23 45 67 89 or 01.23.45.67.89 or +33 1 23 45.67.89
+	public const PHONE_INTL_FRIENDLY = '^(?:0|\+)(?:[\d \(\)\-\.]){6,19}(?:\d){1}$'; //sample +11 (0)12.3456-789
+	public const PHONE_FR = '^(?:0|\+33[ ]?)(?:\d[ \.]?)(?:\d\d[ \.]?){3}(?:\d\d)$'; //sample 0123456789 or 01 23 45 67 89 or 01.23.45.67.89 or +33 1 23 45.67.89
 	public const PHONE_FR_LANDLINE = '^(?:0|\+33[ ]?)(?:[1-59][ \.]?)(?:\d\d[ \.]?){3}(?:\d\d)$'; 
 	public const PHONE_FR_MOBILE = '^(?:0|\+33[ ]?)(?:[67][ \.]?)(?:\d\d[ \.]?){3}(?:\d\d)$';
 	public const PHONE_NL = '^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$';
 	public const PHONE_US ='^([0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$';
 	
-	//Companies ids
+	// Companies ids
 	public const APE_FR = '^\d{4}[A-Z]{1}$';
 	public const EORI_FR = '^FR\d{14}$';
 	public const NAF_FR = '^(?:\d{2}|\d{2}\.\d{1,2}[A-Z]?|\d{2}\.\d{2}\.\d{1,2})$';
@@ -63,7 +63,7 @@ class regexp {
 	public const STREETNO_DE = '^(?:[A-ZÄÖÜ][a-zäöüß]+(([.] )|( )|([-])))+[1-9][0-9]{0,3}[a-z]?$';
 	public const STREETNO_US = '^((\d{1,6}\-\d{1,6})|(\d{1,6}\\\d{1,6})|(\d{1,6})(\/)(\d{1,6})|(\w{1}\-?\d{1,6})|(\w{1}\s\d{1,6})|((P\.?O\.?\s)((BOX)|(Box))(\s\d{1,6}))|((([R]{2})|([H][C]))(\s\d{1,6}\s)((BOX)|(Box))(\s\d{1,6}))?)$';
 	
-	// All EU VAT code per country
+	// VAT codes
 	public const VAT_EU = '^(DE\d{9}|ATU\d{8}|BE0\d{9}|BG\d{9,10}|CY[0-9A-Z]{9}|HR\d{11}|DK\d{8}|ES[0-9A-Z]{1}\d{7}[0-9A-Z]{1}|EE\d{9}|FI\d{8}|FR\d{11}|EL\d{9}|HU\d{8}|IE\d{7}[A-Z]{1,2}|IT\d{11}|LV\d{11}|LT\d{9,12}|LU\d{8}|MT\d{8}|NL[\dB]{11}|PL\d{10}|PT\d{9}|CZ\d{8,10}|RO\d{2,10}|GB\d{9}|SK\d{10}|SI\d{8}|SE\d{10}01)$';
 	public const VAT_AT = '^ATU\d{8}$';
 	public const VAT_BE = '^BE0\d{9}$';
@@ -100,10 +100,10 @@ class regexp {
 	public const ZIPCODE_IT = '^(?:V-|I-)?\d{5}$';
 	public const ZIPCODE_US = '^[0-9]{5}(?:-[0-9]{4})?$';
 	
-	// hash
-	public const HASH_MD5 = '^[a-fA-F0-9]{32}$';
-	public const HASH_SHA1 = '^[a-fA-F0-9]{40}$';
-	public const HASH_SHA256 = '^[A-Fa-f0-9]{64}$';
+	// Hashes
+	public const HASH__MD5 = '^[a-fA-F0-9]{32}$';
+	public const HASH__SHA1 = '^[a-fA-F0-9]{40}$';
+	public const HASH__SHA256 = '^[A-Fa-f0-9]{64}$';
 	
 }
 
